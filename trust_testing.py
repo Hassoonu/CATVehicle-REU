6a8862685f774a2c6cd6db270a2e1f693a3575e1
Kmcillece <135761203+Kmcillece@users.noreply.github.com>
2023-06-22 14:47:42 -0700

    fixed the test

--- trust_testing.py
+++ trust_testing.py
@@ -14,7 +14,7 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
-MAX_STEP = 1000
+MAX_STEP = 4000
 CLAIMING_VEHICLE = 'v0'
 VERIFYING_VEHICLE = 'v1'
 
@@ -45,15 +45,29 @@ def plot_data():
     plt.tight_layout()  # adjust the subplot layout to make it more readable
     plt.show()
 
-def choose_behavior(vid):
-    plexe = Plexe()
+def choose_behavior(plexe, vid):
     # vid: vehicle id
-    # Change lane
-    new_lane = random.randint(0, 2)
-    plexe.set_fixed_lane(vid, new_lane, False)
-    #Plexe.plexe.set_fixed_lane(CLAIMING_VEHICLE, lane=new_lane, safe=False)
-    # speed up
-    # slow down
+    # rand number decides which action the vehicle takes
+    plexe.set_fixed_acceleration(vid, True, 0) # remove any acc
+
+    action = random.randint(0, 3)
+    if (action == 0):
+        # Change lane
+        new_lane = random.randint(0, 3)
+        print(f"changing to lane {new_lane}")
+        plexe.set_fixed_lane(vid, new_lane, False)
+    elif (action == 1):
+        # accelerate
+        print("accelerating")
+        plexe.set_fixed_acceleration(vid, True, 6)
+    elif (action == 2):
+        # decelerate
+        print("decelerating")
+        plexe.set_fixed_acceleration(vid, True, -6)
+    elif (action == 3):
+        # do nothing
+        print("no action")
+        return
 
 def main(real_engine, setter=None):
 
@@ -61,7 +75,7 @@ def main(real_engine, setter=None):
     plexe = Plexe()
     traci.addStepListener(plexe)
     step = 0
-    random.seed(1)
+    random.seed()
     while running(False, step, max_step=MAX_STEP):
 
         traci.simulationStep()
@@ -72,15 +86,14 @@ def main(real_engine, setter=None):
                 data[vid]["accelerations"].append(traci.vehicle.getAcceleration(vid))
                 data[vid]["velocities"].append(traci.vehicle.getSpeed(vid))
         elif step == 1:
-            add_vehicle(plexe, CLAIMING_VEHICLE, 140, 1, 25, "vtypeauto")
-            add_vehicle(plexe, VERIFYING_VEHICLE, 250, 0, 20, "passenger2")
+            add_vehicle(plexe, CLAIMING_VEHICLE, 140, 1, 25, "passenger")
+            add_vehicle(plexe, VERIFYING_VEHICLE, 250, 0, 20, "passenger")
             traci.gui.trackVehicle("View #0", VERIFYING_VEHICLE)
-            traci.gui.setZoom("View #0", 50000)
+            traci.gui.setZoom("View #0", 60000)
 
         # insert vehicle behaviors every interval
         if step % 400 == 1:
-            #choose_behavior(VERIFYING_VEHICLE)
-            plexe.set_fixed_lane(VERIFYING_VEHICLE, 1, False)
+            choose_behavior(plexe, VERIFYING_VEHICLE)
 
         step += 1
 
